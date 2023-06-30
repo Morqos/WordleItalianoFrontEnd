6c367be39a6cfaadec744fac191c30a4e5b742df
root <root@localhost.localdomain>
2023-06-30 15:50:29 +0000

    Set up blank HomePage

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+
+import './Style/index.css';
+
+import HomePage from './Pages/HomePage';
+import Layout from './PageComponents/Layout'
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
 root.render(
   <React.StrictMode>
-    <App />
+    <App/>
   </React.StrictMode>
 );
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout/>}>
+          <Route index element={<HomePage/>} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
